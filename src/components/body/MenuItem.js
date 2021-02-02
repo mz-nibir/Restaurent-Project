1658fd46a7da7f639f151dfd9e3355955d9de4a9
Nibir <mz.nibir@gmail.com>
2021-02-02 16:41:23 +0600

    Designing Menu Item

--- src/components/body/MenuItem.js
+++ src/components/body/MenuItem.js
@@ -1,8 +1,20 @@
+import { Card, CardBody, CardImg, CardImgOverlay, CardTitle } from "reactstrap"
 
 const MenuItem = (props) => {
-    console.log(props);
+
     return (
         <div>
+            <Card style={{ margin: "10px" }}>
+                <CardBody>
+                    <CardImg width="100%" alt={props.name} src={props.dish.image}
+                        style={{ opacity: "0.5" }} />
+                    <CardImgOverlay>
+                        <CardTitle>{props.dish.name}</CardTitle>
+                    </CardImgOverlay>
+
+                </CardBody>
+
+            </Card>
 
         </div>
     );
