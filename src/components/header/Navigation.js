dd834f8a0a05cf22389d8a7f97f8bb044ca2b7f0
Nibir <mz.nibir@gmail.com>
2021-02-10 22:12:12 +0600

    navbar Toggle style added

--- src/components/header/Navigation.js
+++ src/components/header/Navigation.js
@@ -1,22 +1,69 @@
 
-import { Navbar, NavbarBrand } from 'reactstrap'
 
-const Navigation = () => {
+import { Component } from 'react'
+import { Link } from 'react-router-dom'
+import { Nav, Navbar, NavbarBrand, NavbarToggler, NavItem, Collapse } from 'reactstrap'
 
-    return (
-        <div>
-            <Navbar dark color="dark">
-                <div className="container">
-                    <NavbarBrand href="/">Bohubrihi Restaurant</NavbarBrand>
+class Navigation extends Component {
 
-                </div>
+    constructor(props) {
+        super(props);
+        this.state = {
+            inNavOpen: false
+
+        }
+
+    }
+    navToggle = () => {
+        this.setState({
+            inNavOpen: !this.state.inNavOpen
+        })
+    }
+
+    render() {
+
+        return (
+            <div>
+                <Navbar dark color="dark" expand="sm">
+                    <div className="container">
+                        <NavbarToggler onClick={this.navToggle} />
+                        <NavbarBrand href="/">Bohubrihi Restaurant</NavbarBrand>
+                        <Collapse isOpen={this.state.inNavOpen} navbar>
+
+                            <Nav className="mr-auto" navbar>
+
+                                <NavItem>
+                                    <Link to="/" className="nav-link active">Home</Link>
+                                </NavItem>
+
+                                <NavItem>
+                                    <Link to="/menu" className="nav-link">Menu</Link>
+                                </NavItem>
+
+                                <NavItem>
+                                    <Link to="/about" className="nav-link">About</Link>
+                                </NavItem>
+
+                                <NavItem>
+                                    <Link to="/contact" className="nav-link active">Contact</Link>
+                                </NavItem>
 
 
-            </Navbar>
 
-        </div>
-    )
+                            </Nav>
 
+                        </Collapse>
+
+                    </div>
+
+
+                </Navbar>
+
+            </div>
+        )
+
+
+    }
 
 }
 
