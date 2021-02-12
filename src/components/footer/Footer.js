8284b4ff231f8e4f1729b0f84b03c09eed3908f8
Nibir <mz.nibir@gmail.com>
2021-02-12 17:13:20 +0600

    footer component update

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -3,7 +3,14 @@
 const Footer = () => {
 
     return (
-        <div>
+        <div className="footer">
+            <div className="container">
+                <div className="row justify-content-center" >
+                    <div className="col-auto">
+                        <p>Bohubrihi Restaurant &#169; Copyright 2021 </p>
+                    </div>
+                </div>
+            </div>
 
         </div>
     );
